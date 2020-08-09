e563132fa8071e2f438520039c83df8e6951ca44
CCEJaye <charlesjaye.uk@gmail.com>
2020-08-09 06:00:35 +0100

    Implemented texture atlas, started custom shader

---
+++ Game/Assets/Scripts/TextureAtlas.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureAtlas : MonoBehaviour
+{
+    public Texture2D AtlasTexture;
+
+    public const int AtlasSize = 2048;
+    public const int Size = 8;
+    public const int TextureSize = AtlasSize / Size;
+    public const float FloatOffset = 1f / Size;
+    public static readonly Vector2 VectorOffset = new Vector2(FloatOffset, FloatOffset);
+
+    private List<Vector2> TextureVectors = new List<Vector2>();
+
+    public void Start()
+    {
+        GenerateVectors();
+    }
+
+    public Vector4 GetTextureBounds(Vector2 textureVector)
+    {
+        Vector2 xy = textureVector * VectorOffset;
+        return new Vector4(xy.x, xy.y, xy.x + FloatOffset, xy.y + FloatOffset);
+    }
+
+    public Vector2 GetUVForVertex(Vector2 realPos, int textureId)
+    {
+        if (TextureVectors.Count == 0) GenerateVectors();
+        return GetUVForVertex(realPos, TextureVectors[textureId]);
+    }
+
+    public Vector2 GetUVForVertex(Vector2 realPos, Vector2 textureVector)
+    {
+        if (realPos.y > -1f && realPos.y < 0f)
+        {
+            bool test = true;
+        }
+        if (TextureVectors.Count == 0) GenerateVectors();
+        Vector4 textureBounds = GetTextureBounds(textureVector);
+        Vector2 uv = new Vector2(
+            Mathf.Repeat(realPos.x, FloatOffset) + textureBounds.x,
+            Mathf.Repeat(realPos.y, FloatOffset) + textureBounds.y);
+
+        return uv;
+    }
+
+    private void GenerateVectors()
+    {
+        for (int x = 0; x < Size; x++)
+        {
+            for (int y = 0; y < Size; y++)
+            {
+                TextureVectors.Add(new Vector2(x, y));
+            }
+        }
+    }
+}
---
+++ Game/Assets/Shaders/TextureBlend.shader
@@ -0,0 +1,54 @@
+﻿Shader "Custom/TextureBlend"
+{
+    Properties
+    {
+        _Tex1("Texture1", 2D) = "white" {}
+        _Tex2("Texture2", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _Tex1;
+            float4 _Tex1_ST;
+            sampler2D _Tex2;
+            float4 _Tex2_ST;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _Tex1);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = lerp(tex2D(_Tex1, i.uv), tex2D(_Tex2, i.uv), (_SinTime.w + 1) / 2);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
